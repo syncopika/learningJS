35aace817076c7d3a96e9f8eb9d7afbeaf05beb2
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-08-30 14:24:07 -0400

    Update and rename freecodecamp/roman-numeral-converter.js to freecodecamp/bonfires/roman-numeral-converter.js

--- freecodecamp/bonfires/roman-numeral-converter.js
+++ freecodecamp/bonfires/roman-numeral-converter.js
@@ -1,7 +1,9 @@
 //i admit i needed to go to stack overflow for help
+//see here: http://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
 //this is a great opportunity to practice recursion!!
 
 function convert(num) {
+  //if the number to be converted is larger than 30, modifications will have to be added
   if(num >= 30){return "XXX" + convert(num-30);}  //keep subtracting to fill up a string with the appropriate numerals!
   if(num >= 20){return "XX" + convert(num-20);}
   if(num >= 10){return "X" + convert(num-10);}
