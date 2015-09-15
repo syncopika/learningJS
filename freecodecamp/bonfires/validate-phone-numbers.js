a7b7e3270a77cd691aefdbc3cd91f9e2f2e2719e
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-09-15 18:52:40 -0400

    Update validate-phone-numbers.js

--- freecodecamp/bonfires/validate-phone-numbers.js
+++ freecodecamp/bonfires/validate-phone-numbers.js
@@ -4,6 +4,7 @@ function telephoneCheck(str) {
   
   //in regex, .? means optional. the d{#} indicates how many digits to be expected consecutively
   //the [)s-]? means either a ')', space, or hyphen could show up (but not necessarily).
+  //also, I noticed it was important to have the ^ at the front and $ at the back, just to designate a start and end point I guess?
   
   var a = /^[1].?.?\d{3}.?.?\d{3}.?.?\d{4}$/g;
   var b = /^[^a-zA-Z0-9]?\d{3}[)s-]?\d{3}[\s-]?\d{4}$/g;
