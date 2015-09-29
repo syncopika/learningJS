9a4e9b7cff6ee5ebcae17064e0393a4ac5318fcc
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-09-28 23:11:49 -0400

    Create longest-lines.js

---
+++ codeeval/longest-lines.js
@@ -0,0 +1,79 @@
+//can't seem to get this to work but I think it's cause I'm misunderstanding how the file reading works... :/
+//the general process though I think works. I tested a string a like this: 
+
+//EXAMPLE TEST
+
+var a = '2 \n hey there \n hola \n ok hi \n no'
+var table = {};
+a = (a.split('\n'))
+var n = a[0];
+for(i=1;i<a.length;i++){
+  a[i] = a[i].trim();
+  if(table[i] === undefined){
+    table[i] = a[i].length;
+  }
+}
+console.log(table)
+var b = [];
+for(property in table){
+  b.push(table[property]);
+}
+b = b.sort().reverse();
+var c = [];
+for(i=0;i<b.length;i++){
+  for(property in table){
+    if(table[property] === b[i]){
+      c.push(a[property]);
+    }
+  }
+}
+for(i=0;i<n;i++){
+console.log(c[i])
+}
+
+//output:
+//hey there
+//ok hi
+
+/////END EXAMPLE
+
+var a = [];
+var n;
+var table = {};
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+if (line != "") {
+if(typeof line === 'number'){
+n = line;
+}
+else{
+a.push(line);
+}
+}
+});
+
+for(i=0;i<a.length;i++){
+  a[i] = a[i].trim();
+  if(table[i] === undefined){
+    table[i] = a[i].length;
+  }
+}
+
+var b = [];
+for(property in table){
+  b.push(table[property]);
+}
+b = b.sort().reverse();
+
+var c = [];
+for(i=0;i<b.length;i++){
+  for(property in table){
+    if(table[property] === b[i]){
+      c.push(a[property]);
+    }
+  }
+}
+
+for(i=0;i<n;i++){
+console.log(c[i])
+}
