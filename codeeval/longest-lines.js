960913c96f2e0c0d109d218a9a42627797984af4
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-09-29 17:43:17 -0400

    Update longest-lines.js

--- codeeval/longest-lines.js
+++ codeeval/longest-lines.js
@@ -77,3 +77,48 @@ for(i=0;i<b.length;i++){
 for(i=0;i<n;i++){
 console.log(c[i])
 }
+
+
+//ok here's a partial solution
+var a = [];
+var n = 0;
+var table = {};
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+if (line != "") {
+if(typeof parseInt(line) === 'number' && line.length === 1){ //make sure line.length is equal to 1!
+n += line;
+}
+else{
+a.push(line);
+}
+}
+});
+
+for(i=1;i<a.length;i++){
+  a[i] = a[i].trim();
+  if(table[i] === undefined){
+    table[i] = a[i].length;
+  }
+}
+
+var b = [];
+for(property in table){
+  b.push(table[property]);
+}
+b = b.sort().reverse();
+
+var c = [];
+for(i=0;i<b.length;i++){
+  for(property in table){
+    if(table[property] === b[i]){
+      c.push(a[property]);
+    }
+  }
+}
+
+var i = 0;   //for some reason I'm unsure of right now, the while loop gave me some success. :/
+while(i<n){
+console.log(c[i]);
+n++;
+}
