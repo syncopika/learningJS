22c83ec7dbbf1e11c5fb203b8aa7c94956976f13
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-05 21:37:53 -0400

    Update reverse-and-add.js

--- codeeval/reverse-and-add.js
+++ codeeval/reverse-and-add.js
@@ -1,4 +1,6 @@
-//works, but stack size exceeded in codeeval. maybe the integer given is already a string?
+//works, but stack size exceeded in codeeval. 
+//try a number like 196. stack size will be exceeded.
+//it's weird though cause we're supposed to assume that there will be an answer!! grrrr
 
 var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
@@ -25,3 +27,42 @@ else{
 palindrome(line);
     }
 });
+
+//score of 80
+//codeeval instructions kinda suck -__-
+
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line != "") {
+//set tally to 0 for each new number
+var tally = 0;
+
+//set up function
+function palindrome(number){
+var a = number;
+a = a.toString();
+var b = '';
+for(i=a.length-1;i>=0;i--){
+  b += a[i];
+}
+if(tally === 100){
+  //move on
+  console.log('no palindrome')
+}
+else{
+if(parseInt(b) === parseInt(a)){
+  console.log(tally + ' ' + a);
+}
+else{
+  a = parseInt(a) + parseInt(b);
+  tally++;
+  palindrome(a);
+}
+}
+}
+
+
+ palindrome(line);
+    }
+});
+
