7e5137b2ce276bea63d8ee13e800a7d065d92294
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-05 22:40:17 -0400

    Update reverse-and-add.js

--- codeeval/reverse-and-add.js
+++ codeeval/reverse-and-add.js
@@ -33,8 +33,8 @@ palindrome(line);
 
 var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-    if (line != "") {
-//set tally to 0 for each new number
+if (line != "") {
+//set tally to 0 for each new number =>note that this actually does not occur (i.e. tally will accumulate and carry over)
 var tally = 0;
 
 //set up function
@@ -67,3 +67,36 @@ else{
     }
 });
 
+//ahh i've got it! error is indeed partially mine
+//compare the answer for if number = 195 for the program below and the one above ;)
+//sadly, the one below still does not yield 100% correct...
+
+var tally = 0;
+
+function palindrome(number){
+var a = number;
+a = a.toString();
+var b = '';
+for(i=a.length-1;i>=0;i--){
+  b += a[i];
+}
+if(tally === 100){
+  //move on
+  //this tally reset is important
+  tally = 0;
+  console.log('')
+}
+else{
+if(parseInt(b) === parseInt(a)){
+  console.log(tally + ' ' + a);
+  //but also a tally reset here is necessary!
+  //otherwise the tally count will carry over for the next number
+  tally = 0;
+}
+else{
+  a = parseInt(a) + parseInt(b);
+  tally++;
+  palindrome(a);
+}
+}
+}
