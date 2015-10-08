579b097d8d01cb214f67bb615d2b260886a8e61e
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-07 21:12:13 -0400

    Update n-mod-m.js

--- codeeval/n-mod-m.js
+++ codeeval/n-mod-m.js
@@ -1,7 +1,7 @@
 var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
 if (line != "") {
-var a = line.split(',');
+var a = line.split(','); //i.e. line = '20,6'
 var b = parseInt(a[1]);
 a = parseInt(a[0]);
 console.log(a-(b*Math.floor(a/b)))
