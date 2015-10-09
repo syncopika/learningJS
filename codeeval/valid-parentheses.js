6f660989dc3661e730485ccb4110d1e8094de119
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-09 17:27:12 -0400

    Create valid-parentheses.js

---
+++ codeeval/valid-parentheses.js
@@ -0,0 +1,16 @@
+//partial solution...
+
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line != "") {
+var a = line;
+var b = a.indexOf('(')
+var c = a.indexOf(')')
+if(a[b-1] === a[c+1]){
+  console.log('True');
+}
+else{
+  console.log('False');
+}
+    }
+});
