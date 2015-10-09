7627863f52982ed633ef0f1bdbf3c310761d281b
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-09 16:59:22 -0400

    Update multiples-of-a-number.js

--- codeeval/multiples-of-a-number.js
+++ codeeval/multiples-of-a-number.js
@@ -5,7 +5,7 @@ var a = line; //i.e. line = '13,8' output should be 16.
 a = a.split(',');
 var b = parseInt(a[1]);
 a = parseInt(a[0]);
-while(b < a){
+while(b <= a){ //the equal sign is important!
   b = b*2;
 }
 console.log(b)
