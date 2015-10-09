d6579028299db4fc5baac2e68d06a82bbd8a8fba
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-09 17:28:22 -0400

    Update valid-parentheses.js

--- codeeval/valid-parentheses.js
+++ codeeval/valid-parentheses.js
@@ -3,7 +3,7 @@
 var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     if (line != "") {
-var a = line;
+var a = line; //i.e. line = '([)]' - should be False, '()' is True
 var b = a.indexOf('(')
 var c = a.indexOf(')')
 if(a[b-1] === a[c+1]){
