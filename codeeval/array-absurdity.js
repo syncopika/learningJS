c78068854a39c9cbacda90ef111e3b77a086a421
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-12 18:35:34 -0400

    Update array-absurdity.js

--- codeeval/array-absurdity.js
+++ codeeval/array-absurdity.js
@@ -1,4 +1,7 @@
 //my favorite kind of technique! (i think they're called hash tables?)
+//example input: '20;0,1,10,3,2,4,5,7,6,8,11,9,15,12,13,4,16,18,17,14' 
+//output: '4'
+
 
 var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
