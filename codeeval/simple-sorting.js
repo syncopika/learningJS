e39ed7546435f6ff1eb5d94f392f32419a2a921c
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-12 18:29:02 -0400

    Update simple-sorting.js

--- codeeval/simple-sorting.js
+++ codeeval/simple-sorting.js
@@ -1,5 +1,8 @@
 //i wouldn't call this simple...:/
 
+//example input = '-37.507 -3.263 40.079 27.999 65.213 -55.552'
+//output should be: '-55.552 -37.507 -3.263 27.999 40.079 65.213'
+
 function compare(number1, number2){
   return number1 - number2;
 }   
