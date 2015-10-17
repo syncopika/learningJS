3f1a508df65b9e96055e0e6531047c93b8c1a37c
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-10-17 17:29:27 -0400

    Create calculate-distance.js

---
+++ codeeval/calculate-distance.js
@@ -0,0 +1,21 @@
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line != "") {
+var a = line.split(' '); //line = '(25, 4) (1, -6)'
+var b = [];
+for(i=0;i<a.length;i++){
+   var digit = '';
+   for(j=0;j<a[i].length;j++){
+     if(a[i][j].match(/[0-9]/g) !== null || a[i][j].match(/-/g) !== null){
+       digit += a[i][j];
+     }
+   }
+   b.push(digit);
+}
+var deltaY = parseInt(b[3]) - parseInt(b[1]); 
+var deltaX = parseInt(b[2]) - parseInt(b[0]);
+console.log(Math.sqrt((deltaY*deltaY) + (deltaX*deltaX)));
+    }
+});
+    }
+});
