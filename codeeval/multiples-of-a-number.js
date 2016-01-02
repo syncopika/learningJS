4fdd998f273017ada34eec042392c5504afd3dec
Nick H <syncopika@users.noreply.github.com>
2016-01-02 18:04:24 -0500

    Update multiples-of-a-number.js

--- codeeval/multiples-of-a-number.js
+++ codeeval/multiples-of-a-number.js
@@ -13,3 +13,31 @@ while(b <= a){ //the equal sign is important!
 console.log(b)
     }
 });
+
+//6:03 pm est 010216
+//you're an idiot...-_- there were quite a few things you missed...see below...-___-
+//this one is 100% btw...
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+
+if (line != "") {
+var a = line;
+a = a.split(',');
+var b = parseInt(a[1]);
+a = parseInt(a[0]);
+var c = b;
+
+//add the second number to itself each time, not multiply by 2!! 
+while(b < a){ 
+  b = b+c;
+}
+
+//what if both numbers are the same!?
+if(a===c){
+  console.log(a);
+}
+else{
+console.log(b)
+}
+    }
+});
