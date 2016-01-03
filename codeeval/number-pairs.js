6baf2c84bd6ee9c6ab28283940151446e134c11f
Nick H <syncopika@users.noreply.github.com>
2016-01-03 15:44:00 -0500

    Update number-pairs.js

--- codeeval/number-pairs.js
+++ codeeval/number-pairs.js
@@ -27,3 +27,34 @@ if(ans === ''){
 console.log(ans)
     }
 });
+
+//solved!
+
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line != "") {
+var a  = line.split(';');
+var a1 = a[0].split(',');
+var b = '';
+var ans = '';
+for(i=0;i<a1.length;i++){
+  var c = '';
+  for(j=0;j<a1.length;j++){
+   if(a1[i] !== a1[j]){  //I forgot this here! try making line = '1,2,4,5;10' without this if statement. 
+    if(parseInt(a1[i]) + parseInt(a1[j]) === parseInt(a[1])){
+      c += a1[i] + ',' + a1[j] + ';';
+    }
+   }
+  }
+  b += c;
+}
+
+for(i=0;i<b.length/2 - 1;i++){
+  ans += b[i];
+}
+if(ans === ''){
+  ans = 'NULL';
+}
+console.log(ans)
+    }
+});
