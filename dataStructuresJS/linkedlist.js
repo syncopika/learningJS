1a7ef7ac941e1a508f0217f5af9318569a8423af
Nicholas H <syncopika@users.noreply.github.com>
2016-10-23 13:32:37 -0400

    Update linkedlist.js

--- dataStructuresJS/linkedlist.js
+++ dataStructuresJS/linkedlist.js
@@ -1,3 +1,5 @@
+//a linked list data structure
+
 function LinkedList(){
 
 	//initial state
@@ -29,7 +31,7 @@ function LinkedList(){
 			//make sure tail also points to new node
 			this.tail = n;
 		}else{
-			n.next = head;
+			n.next = this.head;
 			this.head = n;
 		}
 	}
@@ -44,6 +46,42 @@ function LinkedList(){
 		}
 		this.tail = n;
 	}
+	 
+	//insert a node AFTER a certain node with the passed data
+	//will not work on adding to tail nodes
+	//only adds once
+	this.insertNode = function(target, data){
+		var curr = this.head;
+		var n = new Node(data);
+		//then go through linked list
+		while(curr !== null){
+			//if we reach a node that has the target data
+			if(curr.data === target){
+				//insert the new node after the target node
+				n.next = curr.next;
+				curr.next = n;
+				break;
+			}
+			curr = curr.next;
+		}
+	}
+	
+	//print the linked list
+	this.printList = function(){
+		var curr = this.head;
+		var string = "";
+		if(curr === null){
+			string += "null";
+			return string;
+		}else{
+			while(curr !== null){
+				string += curr.data + " ";
+				curr = curr.next;
+			}
+			return string;
+		}
+	}
+	
 }
 
 function Node(data){
@@ -55,9 +93,18 @@ function Node(data){
 //TESTING
 var a = new LinkedList();
 
+a.addNodeToHead(1);
+a.addNodeToTail(2);
+a.addNodeToHead(5);
+a.insertNode(1, 3);
+
+console.log(a.printList());
+//so list should look like "5, 1, 3, 2"
+
+/*
 console.log(a.getHead())
 a.addNodeToHead("hey");
 console.log(a.getHead())
 a.addNodeToTail("hello");
 console.log(a.getTail())
-
+*/
