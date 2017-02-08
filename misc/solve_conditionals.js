728e0d85433a9761799bf1e6eca7f4764a009f0f
Nicholas H <syncopika@users.noreply.github.com>
2017-02-07 23:36:43 -0500

    Create solve_conditionals.js

---
+++ misc/solve_conditionals.js
@@ -0,0 +1,50 @@
+//simplify a bunch of nested conditionals
+var a = "(0 | (1 | 0)) & (1 & ((1 | 0) & 0))";
+
+function solve(a){
+  
+  //base case
+  if(a.indexOf("(") < 0){
+    if(a.indexOf("|") > 0){
+      var c = a.split("|");
+      var left = parseInt(c[0].trim());
+      var right = parseInt(c[1].trim());
+      return (left || right);
+    }else if(a.indexOf("&") > 0){
+      var c = a.split("&");
+      var left = parseInt(c[0].trim());
+      var right = parseInt(c[1].trim());
+      return (left && right);
+    }else{
+      return parseInt(a);
+    }
+  }
+  
+  var b = a.split("").reverse().join("");
+  //this is index of nested-most left bracket (could use lastIndexOf)
+  var lIndex = a.length - b.indexOf("(") - 1;
+  var rIndex = a.substring(lIndex).indexOf(")") + lIndex;
+  var substr = a.substring(lIndex+1, rIndex);
+
+  var result;
+  //evaluate substr
+  if(substr.indexOf("|") > 0){
+    var numbers = substr.split("|");
+    var left = parseInt(numbers[0].trim());
+    var right = parseInt(numbers[1].trim());
+    result = left || right;
+
+  }else if(substr.indexOf("&") > 0){
+     var numbers = substr.split("&");
+    var left = parseInt(numbers[0].trim());
+    var right = parseInt(numbers[1].trim());
+    result = left && right;
+
+  }
+  //give substr the brackets back
+  substr = "(" + substr + ")"
+  var c = a.substring(0, a.lastIndexOf(substr)) + a.substring(a.lastIndexOf(substr)).replace(substr, result);
+  console.log(c)
+  return get(c);
+}
+console.log(solve(a))
